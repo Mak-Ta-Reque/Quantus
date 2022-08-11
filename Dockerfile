176c4866fcd8dbbfba3d4df586464ba6d3cce971
Md Abdul Kadir <maktareq@gmail.com>
2022-08-11 16:52:56 +0200

    stremlit started

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.9
 EXPOSE 8501
 WORKDIR /app
-COPY requirements_test.txt ./requirements_test.txt
-RUN pip install -r requirements_test.txt
+COPY requirements_streamlit.txt ./requirements_streamlit.txt
+RUN python -m pip install --upgrade pip
+RUN pip install -r requirements_streamlit.txt
 COPY . .
 CMD streamlit run app.py
--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+
 
 st.title('Uber pickups in NNY')
 
+
+
+# Open the file and load the file
+with open('conf/config.yaml') as f:
+    data = yaml.load(f, Loader=SafeLoader)
+
+config = data["config"]
+data_options = config["DATA"]["root"]
 option = st.selectbox(
      'Which dataset do you want to use',
-     ('Email', 'Home phone', 'Mobile phone'))
+     ('N/A', data_options))
 
 
 st.write('You selected:', option)
